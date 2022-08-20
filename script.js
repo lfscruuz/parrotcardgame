83a488ffa086f55fe6f7515e2d3f4c07e462d266
Luís Felipe Cruz <lfscruz2001@gmail.com>
2022-08-20 11:42:55 -0300

    randomizer pronto

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-const quantity = Number(prompt('Quantas cartas (de 2 a 14)?'));
-const cards = document.querySelector('.cards')
+let quantity = Number(prompt('Quantas cartas (de 2 a 14)?'));
+const cards = document.querySelector('.cards');
+const card = ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1'];
+let selectedCards = [];
 
 
+function game(){
+    while (quantity > 14){
+        quantity = Number(prompt('Você só pode escolher até 14 cartas. Quantas cartas (de 2 a 14)?'));
+    }
+    while ((quantity % 2) !== 0) {
+        quantity = Number(prompt('Você só pode escolher um número par de cartas. Quantas cartas (de 2 a 14)?'));
+    }
 
+    index = 0
+    while (index < quantity / 2){
+        selectedCards.push(card[index]);
+        selectedCards.push(card[index]);
+        index++;
+    }
 
-cardQuantity();
+    console.log(selectedCards)
 
-console.log(quantity)
+    function randomize() { 
+        return Math.random() - 0.5; 
+    }
+
+    selectedCards.sort(randomize);
+}
 
 function cardQuantity(){
 
-    let counter = 1;
+    let counter = 0;
 
-    while (counter < quantity + 16){
+    while (counter < quantity){
         cards.innerHTML = cards.innerHTML + (
-            `<div class="card" onclick="chooseCard(this)">
-                carta ${counter}
+            `<div class="card ${selectedCards[counter]}" onclick="chooseCard(this)">
+                carta ${selectedCards[counter]}
             </div>`);
         counter++
     
     }
 }
+
+game();
+cardQuantity();
 
 // const card = document.querySelector('.card')
 // const cardFront = document.querySelector('.cardFront')
