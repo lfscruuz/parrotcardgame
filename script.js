3eb786853f7e675e95cd758427d1867b86c17e25
Luís Felipe Cruz <lfscruz2001@gmail.com>
2022-08-22 12:41:19 -0300

    comparacao de cartas

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ let quantity = Number(prompt('Quantas cartas (de 2 a 14)?'));
 const cards = document.querySelector('.cards');
 let gameCards = [];
 let selectedCards = [];
+let wonCards = [];
 
-const card1 = '<div class="card"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/bobrossparrot.gif" alt="bobrossparrot"></div></div>'
-const card2 = '<div class="card"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/explodyparrot.gif" alt="bobrossparrot"></div></div>'
-const card3 = '<div class="card"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/fiestaparrot.gif" alt="bobrossparrot"></div></div>'
-const card4 = '<div class="card"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/metalparrot.gif" alt="bobrossparrot"></div></div>'
-const card5 = '<div class="card"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/revertitparrot.gif" alt="bobrossparrot"></div></div>'
-const card6 = '<div class="card"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/tripletsparrot.gif" alt="bobrossparrot"></div></div>'
-const card7 = '<div class="card"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/unicornparrot.gif" alt="bobrossparrot"></div></div>'
+const card1 = '<div class="card card1"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/bobrossparrot.gif" alt="bobrossparrot"></div></div>'
+const card2 = '<div class="card card2"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/explodyparrot.gif" alt="bobrossparrot"></div></div>'
+const card3 = '<div class="card card3"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/fiestaparrot.gif" alt="bobrossparrot"></div></div>'
+const card4 = '<div class="card card4"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/metalparrot.gif" alt="bobrossparrot"></div></div>'
+const card5 = '<div class="card card5"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/revertitparrot.gif" alt="bobrossparrot"></div></div>'
+const card6 = '<div class="card card6"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/tripletsparrot.gif" alt="bobrossparrot"></div></div>'
+const card7 = '<div class="card card7"><div class="front ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/front 1.png" alt="front 1"></div><div class="back ${gameCards[counter]}" onclick="chooseCard(this);"><img src="./media/unicornparrot.gif" alt="bobrossparrot"></div></div>'
 const cardList = [card1, card2, card3, card4, card5, card6, card7];
 
 function game(){
@@ -53,6 +54,29 @@ cardQuantity();
 
 function chooseCard(chosenCard){
     chosenCard.classList.toggle('opacity')
+ 
+    let parentCard = chosenCard.parentNode
 
-    console.log(chosenCard)
+    if (selectedCards.includes(parentCard)){
+        selectedCards = [];
+    }else{
+        selectedCards.push(chosenCard)
+    }
+
+    console.log(selectedCards[0].parentNode.classList)
+
+    if (selectedCards.length === 2){
+        if (selectedCards[0].parentNode.classList[1] === selectedCards[1].parentNode.classList[1]){
+            wonCards.push(selectedCards)
+            selectedCards = [];
+        } else if(selectedCards[0].parentNode.classList[1] !== selectedCards[1].parentNode.classList[1]){
+            // selectedCards[0].classList[1].toggle('opacity')
+            // selectedCards[1].classList[1].toggle('opacity')
+            selectedCards.map((card) =>{card.classList.toggle('opacity')})
+            selectedCards = [];
+        }
+    }
+    
+    console.log(wonCards)
+    console.log(selectedCards.length);
 }
